b127b869fbbc8760ff43d69391670891a4b0eef9
syedanehabatool <nehabatool678@gmail.com>
2026-04-25 17:20:04 +0500

    Delete predictor.py

--- predictor.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-def get_revenue_prediction(df):
-    df = df.sort_values('Year')
-    growth_rates = df['Total Revenue'].pct_change().dropna()
-    avg_growth = growth_rates.mean()
-    last_revenue = df['Total Revenue'].iloc[-1]
-    predicted_revenue = last_revenue * (1 + avg_growth)
-    return predicted_revenue, avg_growth
