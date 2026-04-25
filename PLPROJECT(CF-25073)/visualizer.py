53be9dced07b2a38fc99e91b8987c7b81f011f35
syedanehabatool <nehabatool678@gmail.com>
2026-04-25 17:33:17 +0500

    Delete PLPROJECT(CF-25073) directory

--- PLPROJECT(CF-25073)/visualizer.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-import plotly.express as px
-
-def create_colorful_chart(df, x_col, y_col, title):
-    fig = px.line(df, x=x_col, y=y_col, title=title, markers=True)
-    fig.update_traces(line_color='#FF4B4B', line_width=4)
-    fig.update_layout(template="plotly_dark", title_font_size=24)
-    st.plotly_chart(fig, use_container_width=True)
